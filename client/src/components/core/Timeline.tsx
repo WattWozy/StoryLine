0d11ca71c101389efdc867db6750a2fb25c2b483
Erik Zetterberg <erikzbg@gmail.com>
2024-06-23 15:18:35 +0200

    What may well be the ugliest timeline ever conceived

--- client/src/components/core/Timeline.tsx
+++ client/src/components/core/Timeline.tsx
@@ -5,14 +5,31 @@ interface TimelineProps {
   to?: number;
 }
 
-const Timeline: React.FC<TimelineProps> = ({ from, to }) => {
+const getVerticalLines = (from: number, to: number): string => {
+  let lines = '' + from;
+  for (let year = from; year <= to; year++) {
+    if (year % 5 == 0) {
+        lines += ' | '
+    }
+  }
+  lines += to;
+  return lines;
+}
 
-  const currentYear: number = new Date().getFullYear();
+const Timeline: React.FC<TimelineProps> = ({ from, to }) => {
+    
+
+    const currentYear: number = new Date().getFullYear();
+
+    const verticalLines = getVerticalLines(from, currentYear);
+
+
   
   return (
     <>
       <div>From: {from}</div>
       <div>To: {to ? to : currentYear}</div>
+      <span className="text-nowrap overflow-scroll text-clip flex max-w-full max-h-11">{verticalLines}</span>
     </>
   );
 };
