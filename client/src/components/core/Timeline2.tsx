6cd989d5b0052fb7a90a610c0a543827e5500d83
Erik Zetterberg <erik.zetterberg@appliedtechnology.se>
2024-07-25 17:31:06 +0200

    Dynamically sized grid depending on amount of persons and earliest birth year

--- client/src/components/core/Timeline2.tsx
+++ client/src/components/core/Timeline2.tsx
@@ -8,48 +8,68 @@ interface TimeLineProps {
 
 const currentYear = new Date().getFullYear();
 
-const renderPerson = (person: Person, row: number) => {
+const renderPerson = (
+  person: Person,
+  row: number,
+  firstYearOfTimeLine: number,
+  lengthOfTimeLine: number
+) => {
   const { birth, death } = person;
-  const rowStart = ` row-start-${row + 2}`;
-  const colStart = ` col-start-${birth - 2012}`;
-  const colEnd = death ? ` col-end-${death - 2012}` : " col-end-12";
-  console.log(colStart);
-  console.log(colEnd);
-
+  const colStart = birth - firstYearOfTimeLine + 1;
+  const colEnd = death ? death - firstYearOfTimeLine + 1 : lengthOfTimeLine;
+  //necessary to use inline styling since tailwind does not support dynamic styling
   return (
     <div
       key={row}
-      className={"bg-blue-500 h-10" + colStart + colEnd + rowStart}
+      style={{
+        gridRowStart: row + 2,
+        gridColumnStart: colStart,
+        gridColumnEnd: colEnd,
+      }}
+      className="h-10"
     >
-      <div className={"w-full border-t-4 border-red-500"}></div>
-      {/* <div className="whitespace-nowrap border-2 border-gray-200 rounded-md p-2 bg-white text-black text-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
+      <div
+        className={
+          "w-full border-t-4 border-red-500 rounded-r-full rounded-l-full"
+        }
+      ></div>
+      <div className="whitespace-nowrap border-2 border-gray-200 rounded-md p-2 bg-white text-black text-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
         {person.name}
-      </div> */}
+      </div>
     </div>
   );
 };
 
-const getEveryXYears = (X: number) => {
+const getYears = (length: number, X: number) => {
   return Array.from(
-    { length: 12 / X },
+    { length: length / X },
     (_, index) => currentYear - index * X
   ).reverse();
 };
 
 const Timeline2: React.FC<TimeLineProps> = ({ persons }) => {
-  const years = getEveryXYears(1);
+  const earliestBirthYear = persons.reduce((earliest, person) => {
+    return person.birth < earliest ? person.birth : earliest;
+  }, Infinity);
+  const years = getYears(currentYear - earliestBirthYear + 2, 1);
 
   return (
-    <div className="grid grid-rows-5 grid-cols-[repeat(12,_minmax(0,_1fr))] overflow-x-auto bg-white h-screen pt-32">
+    <div
+      style={{
+        gridTemplateRows: `repeat(${persons.length}, minmax(0, 1fr)`,
+        gridTemplateColumns: `repeat(${years.length}, minmax(90px, 1fr)`,
+      }}
+      className="grid grid-rows-5 grid-cols-[repeat(12,_minmax(0,_1fr))] overflow-x-auto bg-white h-screen pt-32"
+    >
       {years.map((year) => (
         <div className="text-6 font-light" key={year}>
           {year}
         </div>
       ))}
 
-      {persons.map((person, row) => (
-        <PersonLine key={row} person={person} row={row} />
-      ))}
+      {persons.map((person, row) =>
+        renderPerson(person, row, years[0], years.length)
+      )}
     </div>
   );
 };
